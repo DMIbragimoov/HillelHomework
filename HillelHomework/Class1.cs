4995147ae6ec1386046f1f58ec5e515ca6e686f8
DMIbragimov <daniilibragimov466@gmail.com>
2024-05-02 22:49:14 +0300

    added methods for changing gender and overrided ToString

--- HillelHomework/Class1.cs
+++ HillelHomework/Class1.cs
@@ -10,15 +10,49 @@ namespace HillelHomework
     {
         public string? FirstName { get; private set; }
         public string? LastName { get; private set; }
-        public byte Age { get; private set; }
-
+        public byte Age { get; init; }
+        public Gender UserGender { get; private set; }
         public enum Gender
         {
-            Male,
+            Male = 1,
             Women,
-            Other
+            Unknown
         }
 
+        public User()
+        {
 
+        }
+        public User(byte age)
+        {
+            Age = age;
+        }
+
+        public void ChangeGender()
+        {
+            Console.WriteLine("Choose gender: 1 - Male, 2 - Women, 3 - Unknown");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            if (Enum.IsDefined(typeof(Gender), userInput)) 
+            {
+                UserGender = (Gender)userInput;
+            }
+            else
+            {
+                Console.WriteLine("Invalid number!");
+                ChangeGender();
+            }
+        }
+ 
+        public override string ToString()
+        {
+            if (Age < 10)
+            {
+                return ($"Hi, my name is {FirstName} and last name {LastName}. I am a baby. I am {UserGender}.");
+            }
+            else 
+            {
+                return ($"Hi, my name is {FirstName} and last name {LastName}. I am {Age} years old. I am {UserGender}.");
+            }
+        }
     }
 }
