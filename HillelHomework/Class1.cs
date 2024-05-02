9465e4b5b75f78a597bec2b8692f2540ec19bb96
DMIbragimov <daniilibragimov466@gmail.com>
2024-05-02 22:18:21 +0300

    added another class with parametres

---
+++ HillelHomework/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HillelHomework
+{
+    public class User
+    {
+        public string? FirstName { get; private set; }
+        public string? LastName { get; private set; }
+        public byte Age { get; private set; }
+
+        public enum Gender
+        {
+            Male,
+            Women,
+            Other
+        }
+
+
+    }
+}
